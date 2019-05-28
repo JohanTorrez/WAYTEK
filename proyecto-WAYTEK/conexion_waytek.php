de9dcd8d741c16c0b633606c0ce61752d14cb9d8
JohanTorrez <49400369+JohanTorrez@users.noreply.github.com>
2019-05-27 21:37:22 -0500

    Update conexion_waytek.php

--- proyecto-WAYTEK/conexion_waytek.php
+++ proyecto-WAYTEK/conexion_waytek.php
@@ -2,17 +2,16 @@
 
 $nombre_servidor="localhost";
 $nombre_usuario="root";
-$password="";
+$contrasena="";
 $nombre_bd="proyecto_waytek";
-$tabla_usuario="usuario";
 
 //crear uan variable de conexion utilizando extension "mysqli"
 
-$conexion=new mysqli($nombre_servidor, $nombre_usuario, $password, $nombre_bd);
+if (!$mysqli=new mysqli($nombre_servidor, $nombre_usuario, $contrasena, $nombre_bd))
+    {
+        echo "Error al conectar la base de datos, Error: " . $sqli->errno . " " . $sqli->error;
+        $sql->set_charset('utf-8');
+    }
 
-if ($conexion->connect_error){
-    echo "Error de conexión de bases de datos ".$conexion->connect_error;
-    exit();
-}
 
 ?>
