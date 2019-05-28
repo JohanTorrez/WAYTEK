76a98d84dc3a595d2a99568b089a531b01b37d3d
JohanTorrez <49400369+JohanTorrez@users.noreply.github.com>
2019-05-27 21:29:13 -0500

    Update insert_vendedor.php

--- proyecto-WAYTEK/insert_vendedor.php
+++ proyecto-WAYTEK/insert_vendedor.php
@@ -1,7 +1,6 @@
 <?php
-include("conexion.php");
-
-if(isset($_POST['nombre'])){
+include("conexion_waytek.php");
+if(isset($_POST['nombre']) && isset ($_POST['documento']) && isset ($_POST['telefono']) && isset ($_POST['ciudad'])){
         $nombre_tienda=$_POST['nombre'];
         $docu_tienda=$_POST['documento'];
         $telefono_tienda=$_POST['telefono'];
@@ -12,16 +11,24 @@ if(isset($_POST['nombre'])){
         $usuario_tienda=$_POST['usuario'];
         $contra_tienda=$_POST['contrasena'];
 
-        $ins_tienda = $sql->query("INSERT INTO tienda(nombre_tienda, docu_tienda, telefono_tienda, direccion_tienda, correo_tienda	nombre_usuario_tienda, contra_tienda, foto_perfil_tienda)VALUES('$nombre_tienda','$docu_tienda', '$telefono_tienda', '$direccion_tienda','$correo_tienda', '$usuario_tienda', '$contra_tienda')");
+        //datos correspondientes a la imagen
+        $nombre_img=$_FILES['imagen']['name'];
+        $archivo=$_FILES['imagen']['tmp_name'];
 
-        if(!$ins_tienda){
-            echo "Registro Exitoso"
+        //ruta a guardar la imagen
+        $ruta="img";
+        $ruta=$ruta."/".$nombre_img;
+
+        //mover la imagen a la carpeta
+        move_uploaded_file($archivo, $ruta);
+
+        $inser_tienda = $mysqli->query("INSERT INTO tienda(nombre_tienda, docu_tienda, telefono_tienda, direccion_tienda, correo_tienda, nombre_usuario_tienda, contra_tienda, foto_perfil_tienda)VALUES('$nombre_tienda','$docu_tienda', '$telefono_tienda', '$direccion_tienda', '$correo_tienda', '$usuario_tienda', '$ruta', '$contra_tienda')");
+        if(!$inser_tienda){
+            echo "Registro fallido - " . ($mysqli->errno). " " . ($mysqli->error);
         }
         else{
-            echo "Registro fallido" . $sql->errorno " " . $sql->error;
+            echo "Registro Exitoso";
         }
       }
-
       
 ?>
-
